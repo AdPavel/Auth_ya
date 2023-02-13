515080b82704e588011b80c3c31c3d6d9b4e4e9d
elena <ep.yudzina@gamil.com>
2023-02-13 19:41:58 +0500

    add models

--- auth/src/database/db_models.py
+++ auth/src/database/db_models.py
@@ -1,4 +1,5 @@
 import uuid
+from sqlalchemy import ForeignKey
 from sqlalchemy.dialects.postgresql import UUID
 from .db import db
 
@@ -12,3 +13,30 @@ class User(db.Model):
 
     def __repr__(self):
         return f'<User {self.login}>'
+
+
+class LogHistory(db.Model):
+    __tablename__ = 'log_history'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), ForeignKey(User.id))
+    user_agent = db.Column(db.String, nullable=False)
+    login_time = db.Column(db.DateTime, nullable=False)
+
+
+class Role(db.Model):
+    __tablename__ = 'roles'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    name = db.Column(db.String, unique=True, nullable=False)
+
+    def __repr__(self):
+        return f'<Role {self.name}>'
+
+
+class UserRole(db.Model):
+    __tablename__ = 'users_roles'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4, unique=True, nullable=False)
+    user_id = db.Column(UUID(as_uuid=True), ForeignKey(User.id))
+    role_id = db.Column(UUID(as_uuid=True), ForeignKey(Role.id))
