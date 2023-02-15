713aab091290a65493e609396e4ef1769b1e2f60
PavelAd <adap2@mail.ru>
2023-02-15 12:51:04 +0400

    ручки для ролей работают

--- auth/src/api/v1/roles.py
+++ auth/src/api/v1/roles.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 import uuid
 from werkzeug.security import generate_password_hash, check_password_hash
-import jwt
+# import jwt
 import datetime
 from functools import wraps
 from flask import Blueprint
@@ -14,13 +14,47 @@ roles = Blueprint('roles', __name__, url_prefix='/roles')
 
 
 @roles.route('/create', methods=['POST'])
-def create_role(current_user):
-    if not current_user.admin:
-        return jsonify({'message': 'Cannot perform that function!'})
+def create_role():
+    # if not current_user.admin:
+    #     return jsonify({'message': 'Cannot perform that function!'})
 
     data = request.get_json()
-    new_role = Role(name=data['name'])
+    role_name = data['name']
+    name = Role.query.filter_by(name=role_name).first()
+    if name:
+        return jsonify({'message': 'This role is exist!'})
+
+    new_role = Role(name=role_name)
     db.session.add(new_role)
     db.session.commit()
 
-    return jsonify({'message': 'New role created!'})
+    return jsonify({'message': f'New role - "{role_name}" created!'})
+
+
+@roles.route('/delete', methods=['DELETE'])
+def delete_role():
+    data = request.get_json()
+    role_for_delete = Role.query.filter_by(id=data['id']).first()
+
+    if not delete_role:
+        return jsonify({'message': 'No role found!'})
+
+    db.session.delete(role_for_delete)
+    db.session.commit()
+
+    return jsonify({'message': 'Role deleted!'})
+
+
+@roles.route('', methods=['GET'])
+def get_all_roles():
+
+    all_roles = Role.query.all()
+    output = []
+
+    for role in all_roles:
+        role_data = {}
+        role_data['id'] = role.id
+        role_data['name'] = role.name
+        output.append(role_data)
+
+    return jsonify({'roles': output})
