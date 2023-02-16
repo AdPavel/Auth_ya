fd9c9d2422ec57e9c8210423bb78c92081e3e415
elena <ep.yudzina@gamil.com>
2023-02-16 14:04:10 +0500

    add login endpoint

--- auth/src/api/v1/account.py
+++ auth/src/api/v1/account.py
@@ -23,3 +23,23 @@ def create_user():
         return Response(response.message, status=HTTPStatus.BAD_REQUEST)
 
 
+@account.route('/login', methods=['POST'])
+def login():
+
+    data = request.get_json()
+    login = data.get('login', None)
+    password = data.get('password', None)
+
+    if not login or not password:
+        return Response('you have to pass login ans password', status=HTTPStatus.BAD_REQUEST)
+
+    response = db_actions.get_user(login, password)
+    if response.success:
+        user = response.obj
+        user_agent = request.headers['user_agent']
+        db_actions.add_record_to_log_history(user, user_agent)
+        return Response(response.message, status=HTTPStatus.OK)
+    else:
+        return Response(response.message, status=HTTPStatus.BAD_REQUEST)
+
+
--- auth/src/database/db_actions.py
+++ auth/src/database/db_actions.py
@@ -1,20 +1,31 @@
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.exc import IntegrityError
-from werkzeug.security import generate_password_hash
+from werkzeug.security import generate_password_hash, check_password_hash
 from pydantic import BaseModel
 from typing import Union
+from datetime import datetime
 
 from .db import db
-from .db_models import User, Role, users_roles
+from .db_models import User, Role, users_roles, LogHistory
 
 
 class ActionResponse(BaseModel):
     success: bool
-    # obj: Union[None, User, Role, users_roles]
+    obj: Union[None, User, Role, LogHistory]
     message: Union[None, str]
 
     class Config:
         arbitrary_types_allowed = True
+
+
+def add_record_to_log_history(user: User, user_agent: str):
+    entry = LogHistory(
+        user_id=user.id,
+        user_agent=user_agent,
+        login_time=datetime.now()
+    )
+    db.session.add(entry)
+    db.session.commit()
 
 
 def create_user(login: str, password: str) -> ActionResponse:
@@ -35,6 +46,29 @@ def create_user(login: str, password: str) -> ActionResponse:
         obj=user,
         message=None
     )
+
+
+def get_user(login: str, password: str) -> ActionResponse:
+    user = User.query.filter_by(login=login).first()
+    if not user:
+        return ActionResponse(
+            success=False,
+            obj=None,
+            message='Пользователь не найден'
+        )
+    if check_password_hash(user.password, password):
+        return ActionResponse(
+            success=True,
+            obj=user,
+            message='{"token": "token"}'
+        )
+    else:
+        return ActionResponse(
+            success=False,
+            obj=None,
+            message='Неправильный пароль'
+        )
+
 
 def set_role(user, superuser_role) -> ActionResponse:
     # user_role = users_roles(user_id=user_id, role_id=role_id)
