c82a4cbec1dd8d82701c7fb2a90f4b9373a0b890
PavelAd <adap2@mail.ru>
2023-02-17 16:02:59 +0400

    добавил аннотацию

--- auth/src/api/v1/roles.py
+++ auth/src/api/v1/roles.py
@@ -1,3 +1,4 @@
+import uuid
 from flask import jsonify, Response
 from http import HTTPStatus
 from flask import Blueprint
@@ -11,7 +12,7 @@ roles = Blueprint('roles', __name__, url_prefix='/roles')
 
 @roles.route('/create', methods=['POST'])
 @db_role_actions.role_required
-def create_role(role_name):
+def create_role(role_name: str):
 
     response = db_role_actions.create_role(role_name)
     if response.success:
@@ -21,7 +22,7 @@ def create_role(role_name):
 
 
 @roles.route('/delete/<uuid:role_id>', methods=['DELETE'])
-def delete_role(role_id):
+def delete_role(role_id: uuid):
     if not role_id:
         return Response('Не указа id роли для удаления', status=HTTPStatus.BAD_REQUEST)
 
@@ -49,7 +50,7 @@ def get_all_roles():
 
 @roles.route('/change/<uuid:role_id>', methods=['PUT'])
 @db_role_actions.role_required
-def change_role(new_name, role_id):
+def change_role(new_name: str, role_id: uuid):
 
     response = db_role_actions.update_role(new_name, role_id)
     if response.success:
@@ -59,7 +60,7 @@ def change_role(new_name, role_id):
 
 
 @roles.route('/<uuid:user_id>', methods=['GET'])
-def get_user_roles(user_id):
+def get_user_roles(user_id: uuid):
     if not user_id:
         Response('Не указан id пользователя', status=HTTPStatus.BAD_REQUEST)
 
@@ -70,7 +71,7 @@ def get_user_roles(user_id):
 
 @roles.route('/<uuid:user_id>/create', methods=['POST'])
 @db_role_actions.role_required
-def set_user_role(role_name, user_id):
+def set_user_role(role_name: str , user_id: uuid):
 
     response = db_role_actions.set_or_del_user_role(user_id, role_name)
     if response.success:
@@ -81,7 +82,7 @@ def set_user_role(role_name, user_id):
 
 @roles.route('/<uuid:user_id>/delete', methods=['DELETE'])
 @db_role_actions.role_required
-def delete_user_role(role_name, user_id):
+def delete_user_role(role_name: str, user_id: uuid):
 
     response = db_role_actions.set_or_del_user_role(user_id, role_name, is_delete=True)
     if response.success:
