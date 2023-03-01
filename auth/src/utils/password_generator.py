c0a850eb7dde56d5464b47e5d0822cbc20457384
PavelAd <adap2@mail.ru>
2023-03-01 15:42:30 +0400

    добавил генерацию пароля для нового пользователя из соц.сетей

---
+++ auth/src/utils/password_generator.py
@@ -0,0 +1,7 @@
+import secrets
+import string
+
+def get_random_password():
+    alphabet = string.ascii_letters + string.digits
+    password = ''.join(secrets.choice(alphabet) for i in range(6))
+    return password
