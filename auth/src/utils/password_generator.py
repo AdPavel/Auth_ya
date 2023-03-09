55d3df209d84c6094fc3425862b12d063dd257ef
elena <ep.yudzina@gamil.com>
2023-03-09 17:18:15 +0500

    add length param to password generator

--- auth/src/utils/password_generator.py
+++ auth/src/utils/password_generator.py
@@ -2,7 +2,7 @@ import secrets
 import string
 
 
-def get_random_password():
+def get_random_password(length=6):
     alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(6))
+    password = ''.join(secrets.choice(alphabet) for i in range(length))
     return password
